946869cfe2a8ad9cbf9245b6d91894fb331b228f
Betty Lam <b.lam4392@gmail.com>
2017-08-08 13:59:18 -0700

    Complete sliding and stepping pieces

---
+++ week2/w2d1/chess/stepping_pieces.rb
@@ -0,0 +1,39 @@
+require_relative 'pieces'
+
+module SteppingPiece
+  def moves(pos)
+    moves = []
+    move_dirs.each do |delta|
+      x = pos[0] + delta[0]
+      y = pos[1] + delta[1]
+      moves << [x, y] if @board.in_bounds?([x, y])
+    end
+    moves
+  end
+end
+
+class King < Piece
+  include SteppingPiece
+
+  def initialize(position, board)
+    super('K', position, board)
+  end
+
+  private
+  def move_dirs
+    [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [-1, -1], [1, -1], [-1, 1]]
+  end
+end
+
+class Knight < Piece
+  include SteppingPiece
+
+  def initialize(position, board)
+    super('N', position, board)
+  end
+
+  private
+  def move_dirs
+    [[-2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2]]
+  end
+end
