819853581ff396227615e3bc93bed006c7d13992
Betty <b.lam4392@gmail.com>
2017-08-21 19:47:56 -0700

    Art Share API

---
+++ week4/w4d1/first_routes/app/models/artwork.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  image_url  :string           not null
+#  artist_id  :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Artwork < ApplicationRecord
+  validates :title, :image_url, :artist_id, presence: true
+  validates :image_url, uniqueness: true
+  validates :title, uniqueness: { scope: :artist_id }
+
+  belongs_to :artist,
+    primary_key: :id,
+    foreign_key: :artist_id,
+    class_name: :User
+
+  has_many :artwork_shares, dependent: :destroy
+
+  has_many :shared_viewers,
+    through: :artwork_shares,
+    source: :viewer
+end
--- week4/w4d1/first_routes/db/seeds.rb
+++ week4/w4d1/first_routes/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create!(username: 'cat')
+u2 = User.create!(username: 'dog')
+u3 = User.create!(username: 'dragon')
+u4 = User.create!(username: 'monkey')
+
+a1 = Artwork.create!(title: 'Cat', image_url: 'cat.jpg', artist_id: u1.id)
+a2 = Artwork.create!(title: 'Dog', image_url: 'dog.jpg', artist_id: u2.id)
+
+s1 = ArtworkShare.create!(artwork_id: a1.id, viewer_id: u2.id)
+s2 = ArtworkShare.create!(artwork_id: a1.id, viewer_id: u3.id)
+s3 = ArtworkShare.create!(artwork_id: a2.id, viewer_id: u3.id)
+s4 = ArtworkShare.create!(artwork_id: a2.id, viewer_id: u4.id)
