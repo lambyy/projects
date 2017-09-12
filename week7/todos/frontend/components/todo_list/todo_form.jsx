aa3ab1af293d266e83116dfe4c9bf1115d331b5e
Betty Lam <b.lam4392@gmail.com>
2017-09-11 17:12:34 -0700

    Complete phase 4: TodoForm

---
+++ week7/todos/frontend/components/todo_list/todo_form.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { uniqueId } from '../../util/util';
+
+class TodoForm extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = { title: "", body: "" };
+
+    this.handleInputTitle = this.handleInputTitle.bind(this);
+    this.handleInputBody = this.handleInputBody.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleInputTitle(event) {
+    event.preventDefault();
+
+    this.setState({ title: event.currentTarget.value });
+  }
+
+  handleInputBody(event) {
+    event.preventDefault();
+
+    this.setState({ body: event.currentTarget.value });
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+
+    const { title, body } = this.state;
+    const { receiveTodo } = this.props;
+    const todo = Object.assign({}, this.state, { id: uniqueId() });
+    // const todo = {
+    //   id: uniqueId(),
+    //   title,
+    //   body,
+    //   done: false
+    // };
+
+    receiveTodo(todo);
+    this.setState({ title: "", body: ""});
+  }
+
+  render() {
+    const { title, body } = this.state;
+
+    return (
+      <div>
+        <label id="todo-title">Title:</label>
+        <br />
+        <input id="todo-title" type="text"
+               value={title}
+               onChange={this.handleInputTitle}></input>
+        <br />
+
+        <label id="todo-body">Body:</label>
+        <br />
+        <textarea id="todo-body"
+                  value={body}
+                  onChange={this.handleInputBody}></textarea>
+        <br />
+
+        <input type="submit"
+               value="Create todo!"
+               onClick={this.handleClick}></input>
+      </div>
+    );
+  }
+}
+
+export default TodoForm;
--- week7/todos/frontend/components/todo_list/todo_list.jsx
+++ week7/todos/frontend/components/todo_list/todo_list.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import TodoListItem from './todo_list_item';
+import TodoForm from './todo_form';
 
 const TodoList = ({ todos, receiveTodo }) => (
-  <ul>
-    {
-      todos.map( todo => (
-        <TodoListItem key={todo.id} todo={todo} />
-      ))
-    }
-  </ul>
+  <div>
+    <ul>
+      {
+        todos.map( todo => (
+          <TodoListItem key={todo.id} todo={todo} />
+        ))
+      }
+    </ul>
+    <TodoForm receiveTodo={receiveTodo}/>
+  </div>
 );
 
 export default TodoList;
